5fc79edc0fc7fedbc1451bea9ac9e3e02383c494
tiago972 <edouard.baudouin@hotmail.fr>
2019-03-11 20:31:15 +0100

    a la bouillette

--- main.c
+++ main.c
@@ -10,9 +10,14 @@ void			uf_striter_callback(char *s)
 
 int		main()
 {		
-
-char		str[] = "Hello";
-
-	printf("%d\n", ft_strequ("", ""));	
+	char	*str;
+	int ret = 0;
+	str = strdup("abc"); /* FIX un faux OK si l'user a mis "if s1 == s2 return 1;" */
+	if ((ret = ft_strequ("", "")) != 1)
+	{
+		printf("Error Line %d, Funct %s : \n\033[31mft_strequ(\"\", \"\").\nExpected ret = \"1\" \
+				but have ret = \"%d\"\033[0m\n", __LINE__ - 2, __func__, ret);
+	}
+	free(str);
 	return (1);
 }
--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -9,6 +9,8 @@ int		ft_strcmp(char *s1, char *s2)
 
 	cpys1 = s1;
 	cpys2 = s2;
+	c1 = *cpys1;
+	c2 = *cpys2;
 	while (*cpys1 || *cpys2)
 	{
 		c1 = *cpys1;
