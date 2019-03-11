325388a48988b29a3e98df0aea2f9e3c391fd9c9
tiago972 <edouard.baudouin@hotmail.fr>
2019-03-11 19:57:12 +0100

    ?

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 #include <sys/mman.h>
 int		main()
 {		
+	char	*str = "Hello les genw";
 	char *s1 = "AAAAAAAAAAAAA";
 	size_t max = strlen(s1);
-	char *i1 = strnstr(s1, s1, max);
-	char *i2 = ft_strnstr(s1, s1, max);
+	char *i1 = strnstr(str,"" , 3);
+	char *i2 = ft_strnstr(str,"", 3);
 	if (i1!= i2)
 		printf("eux %s, moi %s\n", i1, i2);
 	return (0);
