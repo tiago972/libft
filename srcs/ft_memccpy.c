1ddd3796b221eba4da95a03ec83c7612b54a03b5
tiago972 <edouard.baudouin@hotmail.fr>
2019-03-11 18:41:26 +0100

    repare

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -3,10 +3,16 @@
 void	*ft_memccpy(void *dest, const void *src, int c, size_t len)
 {
 	void	*cpy;
+	void	*res;
+	int		n;
 
 	cpy = ft_memchr(src, c, len);
 	if (cpy)
-		return (ft_memcpy(dest, src, cpy - src + 1));
+	{
+		n =  cpy - src + 1;
+		res  = ft_memcpy(dest, src, cpy - src + 1);
+		return (res + n);
+	}
 	ft_memcpy(dest, src, len);
 	return (NULL);
 }
